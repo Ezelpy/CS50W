e68906823d04fc070c4665266b0834e0c51d7d97
Ezelpy <enr98.ezequiel@gmail.com>
2025-07-16 18:41:53 -0300

    Index and Entry Page are working

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 
 from . import util
-
+from markdown2 import Markdown
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
+        # These are the variables that we are giving to the html
         "entries": util.list_entries()
     })
+
+def wiki(request, title):
+    entry = util.get_entry(title)
+    markdown = Markdown()
+    if entry is None:
+        return render(request, "encyclopedia/error.html")
+    else:
+        return render(request, "encyclopedia/wiki.html", {
+            "title": title,
+            "entry": markdown.convert(entry)
+        })
+
+
+
+    
 
