c2a92cc9a7ed3013b7baec1bb962d5df8f2324b8
Ezelpy <enr98.ezequiel@gmail.com>
2025-07-17 20:55:11 -0300

    Search implemented

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from . import util
 from markdown2 import Markdown
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        # These are the variables that we are giving to the html
-        "entries": util.list_entries()
-    })
+    if request.GET.keys():
+        query = request.GET.get('q') 
+        entry = util.get_entry(query)
+        if entry:
+           return HttpResponse(wiki(request, query)) 
+        else:
+            return HttpResponse(searchResults(request, query)) 
+
+    else:
+        return render(request, "encyclopedia/index.html", {
+            # These are the variables that we are giving to the html
+            "entries": util.list_entries()
+        })
 
 def wiki(request, title):
     entry = util.get_entry(title)
@@ -20,7 +29,14 @@ def wiki(request, title):
             "entry": markdown.convert(entry)
         })
 
+def searchResults(request, query):
+    listQuery = []
+    listEntries = util.list_entries()
 
+    for entry in listEntries:
+        if query in entry:
+            listQuery.append(entry)
 
-    
-
+    return render(request, "encyclopedia/searchResults.html", {
+        "listEntries": listQuery
+    })
